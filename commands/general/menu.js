9c1ac85425f0a2e32640d2abad5fcbb5cd911de0
King123XZ <yersonorosco2002@gmail.com>
2025-12-05 13:40:39 -0500

    Update menu.js

--- commands/general/menu.js
+++ commands/general/menu.js
@@ -1,8 +1,9 @@
 module.exports = {
-  name: "menu",
-  alias: ["help", "ayuda"],
-  run: async (client, m) => {
+  command: ["menu", "help", "ayuda"],
 
+  run: async (client, m, { prefix }) => {
+
+    // Enviar la imagen del menú principal
     await client.sendMessage(m.chat, {
       image: { url: "https://i.ibb.co/XxdTkYNq/menu.png" },
       caption: `⧼ 𝐘𝐞𝐫𝐓𝐗 𝐁𝐎𝐓 - 𝐌𝐄𝐍𝐔 𝐇𝐀𝐂𝐊𝐄𝐑 ⧽
@@ -12,12 +13,26 @@ module.exports = {
 🕶️ Versión: 2.0`
     });
 
+    // Botones de categorías
     const buttons = [
-      { buttonId: "menu_descargas", buttonText: { displayText: "📥 Descargas" }, type: 1 },
-      { buttonId: "menu_utilidades", buttonText: { displayText: "🛠 Utilidades" }, type: 1 },
-      { buttonId: "menu_infobot", buttonText: { displayText: "🤖 InfoBot" }, type: 1 },
+      {
+        buttonId: "menu_descargas",
+        buttonText: { displayText: "📥 Descargas" },
+        type: 1
+      },
+      {
+        buttonId: "menu_utilidades",
+        buttonText: { displayText: "🛠 Utilidades" },
+        type: 1
+      },
+      {
+        buttonId: "menu_infobot",
+        buttonText: { displayText: "🤖 InfoBot" },
+        type: 1
+      }
     ];
 
+    // Enviar los botones
     await client.sendMessage(m.chat, {
       text: "Selecciona una categoría:",
       footer: "YerTX Bot",
