20fefc4c74b5c4e39d917486758de71277109088
King123XZ <yersonorosco2002@gmail.com>
2025-12-05 12:23:52 -0500

    Create comandos-descarga.js

---
+++ commands/general/comandos-descarga.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    command: ["ytmp3", "mp3", "youtubeaudio"],
+    description: "Descarga audio de YouTube en 128 kbps",
+    category: "downloader"
+  },
+  {
+    command: ["ytmp3doc", "mp3doc"],
+    description: "Descarga audio de YouTube en documento .mp3",
+    category: "downloader"
+  },
+  {
+    command: ["ytmp4", "mp4", "youtubevideo"],
+    description: "Descarga video de YouTube en 360p",
+    category: "downloader"
+  },
+  {
+    command: ["ytmp4doc", "mp4doc"],
+    description: "Descarga video de YouTube como documento 360p",
+    category: "downloader"
+  },
+  {
+    command: ["tiktok", "tt"],
+    description: "Descarga video de TikTok sin marca de agua",
+    category: "downloader"
+  },
+  {
+    command: ["tiktokdoc", "ttdoc"],
+    description: "Descarga TikTok en documento",
+    category: "downloader"
+  },
+  {
+    command: ["fb", "facebook"],
+    description: "Descarga videos de Facebook",
+    category: "downloader"
+  },
+  {
+    command: ["ig", "instagram"],
+    description: "Descarga reels, historias o publicaciones de Instagram",
+    category: "downloader"
+  },
+  {
+    command: ["capcut"],
+    description: "Descarga plantillas de CapCut",
+    category: "downloader"
+  }
+];
