3a484c105144c87233038ae4522fc5821c661690
King123XZ <yersonorosco2002@gmail.com>
2025-12-05 08:06:39 -0500

    Create comandos-descarga.js

---
+++ commands/general/comandos-descarga.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    command: ["ytmp3", "mp3"],
+    description: "Descarga música de YouTube",
+    category: "downloader"
+  },
+  {
+    command: ["ytmp4", "mp4"],
+    description: "Descarga videos de YouTube",
+    category: "downloader"
+  },
+  {
+    command: ["play", "ytplay"],
+    description: "Reproduce y descarga música de YouTube",
+    category: "downloader"
+  },
+  {
+    command: ["tiktok", "tt"],
+    description: "Descarga videos de TikTok",
+    category: "downloader"
+  }
+];
