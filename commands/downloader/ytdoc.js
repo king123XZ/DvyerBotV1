cf64ca7266695aa26fae72e3384604632c660a54
DevYerZx <yersonorosco2002@gmail.com>
2025-12-29 17:52:38 -0500

    Update ytdoc.js

--- commands/downloader/ytdoc.js
+++ commands/downloader/ytdoc.js
@@ -9,21 +9,24 @@ module.exports = {
       return m.reply("❌ Enlace de YouTube no válido.");
     }
 
-    // Guardamos el enlace
+    // 📦 Cache por usuario (anti-spam)
     global.ytDocCache = global.ytDocCache || {};
-    global.ytDocCache[m.sender] = url;
+    global.ytDocCache[m.sender] = {
+      url,
+      time: Date.now()
+    };
 
     const buttons = [
-      { buttonId: ".ytdocq 360", buttonText: { displayText: "📄 360p" }, type: 1 },
-      { buttonId: ".ytdocq 480", buttonText: { displayText: "📄 480p" }, type: 1 },
-      { buttonId: ".ytdocq 720", buttonText: { displayText: "📄 720p" }, type: 1 }
+      { buttonId: `.ytdocq 360 ${m.sender}`, buttonText: { displayText: "📄 360p" }, type: 1 },
+      { buttonId: `.ytdocq 480 ${m.sender}`, buttonText: { displayText: "📄 480p" }, type: 1 },
+      { buttonId: `.ytdocq 720 ${m.sender}`, buttonText: { displayText: "📄 720p HD" }, type: 1 }
     ];
 
     await client.sendMessage(
       m.chat,
       {
         text: "📥 *Selecciona la calidad del video (DOCUMENTO):*",
-        footer: "YerTX Bot",
+        footer: "Killua-Bot V1.00",
         buttons,
         headerType: 1
       },
@@ -31,4 +34,5 @@ module.exports = {
     );
   }
 };
+
 
