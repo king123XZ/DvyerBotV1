e23901cec6b43cb4b33005914aafe17125706271
DevYerZx <yersonorosco2002@gmail.com>
2025-12-31 18:40:08 -0500

    Create pelicula.js

---
+++ commands/downloader/pelicula.js
@@ -0,0 +1,70 @@
+const axios = require("axios");
+const movies = require("../../lib/movies");
+
+const API_KEY = "TU_API_KEY_AQUI";
+const MAX_MB = 1800;
+
+module.exports = {
+  command: ["pelicula"],
+  category: "downloader",
+
+  run: async (client, m, args) => {
+
+    if (!args[0]) {
+      return m.reply("❌ Usa: .pelicula <número>");
+    }
+
+    const movie = movies.find(v => v.id == args[0]);
+    if (!movie) {
+      return m.reply("❌ Película no encontrada.");
+    }
+
+    // 🔒 SOLO EN CONSOLA
+    console.log("📥 Descargando película");
+    console.log("🎬 Título:", movie.title);
+    console.log("🔗 killuaVip:", movie.url);
+
+    await client.sendMessage(
+      m.chat,
+      {
+        image: { url: movie.image },
+        caption:
+          `🎬 *${movie.title}*\n` +
+          `🎥 ${movie.quality}\n` +
+          `🔐 Contraseña: www.blizzboygames.net\n\n` +
+          `⏳ Descargando...`
+      },
+      { quoted: m }
+    );
+
+    try {
+      const res = await axios.post(
+        "https://api-sky.ultraplus.click/download/mediafire",
+        { url: movie.url },
+        { headers: { apikey: API_KEY } }
+      );
+
+      const file = res.data.result.files[0];
+
+      const stream = await axios.get(file.download, {
+        responseType: "arraybuffer",
+        timeout: 0
+      });
+
+      await client.sendMessage(
+        m.chat,
+        {
+          document: Buffer.from(stream.data),
+          mimetype: "application/octet-stream",
+          fileName: file.name,
+          caption: `🎬 ${movie.title}\n👑 DevYer`
+        },
+        { quoted: m }
+      );
+
+    } catch (err) {
+      console.error(" ERROR:", err.message);
+      m.reply("❌ Error al descargar la película.");
+    }
+  }
+};
