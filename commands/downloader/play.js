f09bed7913e514162fca857695883662ac875bd1
DevYerZx <yersonorosco2002@gmail.com>
2026-01-08 17:49:51 -0500

    Actualizar play.js

--- commands/downloader/play.js
+++ commands/downloader/play.js
@@ -68,7 +68,7 @@ module.exports = {
       const buttons = [
         { buttonId: `.ytaudio ${video.url}`, buttonText: { displayText: "🎵 Audio" }, type: 1 },
         { buttonId: `.ytvideo ${video.url}`, buttonText: { displayText: "🎬 Video" }, type: 1 },
-        { buttonId: `.ytdoc ${video.url}`, buttonText: { displayText: "🎬doc mp4 videos largos" }, type: 1 }
+        { buttonId: `.ytdoc ${video.url}`, buttonText: { displayText: "📂DOC(Videos largos) " }, type: 1 }
       ];
 
       await client.sendMessage(
