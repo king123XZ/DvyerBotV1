5cfe64c000937a2d0d5e59eb3e7d14cc8d7ed7e8
DevYerZx <yersonorosco2002@gmail.com>
2026-01-08 14:09:15 -0500

    Update play.js

--- commands/downloader/play.js
+++ commands/downloader/play.js
@@ -67,8 +67,8 @@ module.exports = {
 
       const buttons = [
         { buttonId: `.ytaudio ${video.url}`, buttonText: { displayText: "🎵 Audio" }, type: 1 },
-        //{ buttonId: `.ytvideo ${video.url}`, buttonText: { displayText: "🎬 Video" }, type: 1 },
-        { buttonId: `.ytdoc ${video.url}`, buttonText: { displayText: "🎬 video mp4" }, type: 1 }
+        { buttonId: `.ytvideo ${video.url}`, buttonText: { displayText: "🎬 Video" }, type: 1 },
+        { buttonId: `.ytdoc ${video.url}`, buttonText: { displayText: "🎬doc mp4 videos largos" }, type: 1 }
       ];
 
       await client.sendMessage(
