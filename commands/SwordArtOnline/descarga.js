59cd00bf289550e8eab49e5150fb16815be5169a
DevYerZx <yersonorosco2002@gmail.com>
2026-01-09 22:35:50 -0500

    Update descarga.js

--- commands/SwordArtOnline/descarga.js
+++ commands/SwordArtOnline/descarga.js
@@ -4,24 +4,26 @@ const animeList = require("../../lib/anime");
 const API_KEY = "dvyer";
 
 module.exports = {
-  command: ["anime_dl"],
+  command: ["sao_dl"],
   category: "anime",
 
   run: async (client, m, args) => {
-    const [animeId, epNum] = args;
-    const anime = animeList.find(a => a.id === animeId);
+    const epNum = Number(args[0]);
+    const anime = animeList.find(a => a.id === "sao");
 
     if (!anime) return client.reply(m.chat, "❌ Anime no encontrado.", m);
 
-    const ep = anime.seasons[0].episodes.find(
-      e => e.ep === Number(epNum)
-    );
+    const ep = anime.seasons[0].episodes.find(e => e.ep === epNum);
 
     if (!ep || ep.url === "xxxx") {
       return client.reply(m.chat, "❌ Episodio no disponible.", m);
     }
 
-    await client.reply(m.chat, `⏳ Descargando ${ep.title}`, m);
+    await client.reply(
+      m.chat,
+      `⏳ Descargando *${anime.title} - ${ep.title}*`,
+      m
+    );
 
     const res = await axios.get(
       "https://api-adonix.ultraplus.click/download/mediafire",
