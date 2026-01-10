a7ca6f702c4a9ade5c4f150686fb57f832ecd045
DevYerZx <yersonorosco2002@gmail.com>
2026-01-10 05:08:39 -0500

    Update update.js

--- commands/owner/update.js
+++ commands/owner/update.js
@@ -30,7 +30,7 @@ module.exports = {
   command: ["update", "actualizar"],
   description: "Actualiza desde GitHub",
   isOwner: true,
-  category: "owner",
+  categoria: "dueño",
   run: async (client, m, args, from, isCreator) => {
     const { exec } = require("child_process");
     const baseDir = path.join(__dirname, "..");
